4a619af407caa19e8dc1f84b3068096d533070cd
hotteokButler <ygs12567@gmail.com>
2023-11-23 00:17:55 +0900

    Fix : nowPlayingdata loader api 연결

--- src/router/load/MainSlData.jsx
+++ src/router/load/MainSlData.jsx
@@ -1,5 +1,5 @@
 export const nowPlayingData = async () => {
-  const res = await fetch('/json/now_playing.json');
+  const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1&api_key=${process.env.REACT_APP_MOVIE_API}`);
 
   if (res.ok) {
     return await res.json();
